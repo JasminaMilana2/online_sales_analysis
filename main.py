a3dd88e22308143fd5733539a33d6870e7ee1b3c
JasminaMilana2 <jasminabul@gmail.com>
2025-03-26 18:05:07 +0100

    Ispravka grešaka u main.py

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+from product_manager import ProductManager
+from product import Product
 from cart import Cart
 
 cart = Cart()
+
+# Kreiranje menadžera proizvoda
+manager = ProductManager()
+
+# Dodavanje proizvoda
+manager.add_product(Product("Laptop", 1000, 5))
+manager.add_product(Product("Telefon", 500, 10))
+manager.add_product(Product("Tablet", 300, 7))
+
+# Prikaz proizvoda i ukupne vrednosti
+manager.display_products()
+print(f"Ukupna vrednost inventara: {manager.total_inventory_value()}$")
 
 # Dodavanje proizvoda u korpu
 cart.add_to_cart(manager.products[0])
