a1236a8e2c2bad60ddfdd13b0297c24f01a63208
JasminaMilana2 <jasminabul@gmail.com>
2025-03-26 17:55:38 +0100

    Dodata funkcionalnost korpe

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from product_manager import ProductManager
-from product import Product
+from cart import Cart
 
-# Kreiranje menadžera proizvoda
-manager = ProductManager()
+cart = Cart()
 
-# Dodavanje proizvoda
-manager.add_product(Product("Laptop", 1000, 5))
-manager.add_product(Product("Telefon", 500, 10))
-manager.add_product(Product("Tablet", 300, 7))
+# Dodavanje proizvoda u korpu
+cart.add_to_cart(manager.products[0])
+cart.add_to_cart(manager.products[1])
+cart.add_to_cart(manager.products[2])
 
-# Prikaz proizvoda i ukupne vrednosti
-manager.display_products()
-print(f"Ukupna vrednost inventara: {manager.total_inventory_value()}$")
+# Prikaz sadržaja korpe i ukupne cene
+cart.display_cart()
+print(f"Ukupna vrednost korpe: {cart.total_cart_value()}$")
